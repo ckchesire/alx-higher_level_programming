bc595f463c4072de4a482f7fe71f18a0b5e5b2bb
ckchesire <christianck.chesire@gmail.com>
2024-09-10 21:56:03 +0300

    Added implementation to generate random signed number and to check if certain condition has been met

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 
 number = random.randint(-10000, 10000)
 len_str = len(str(number))
-last_number = str(number)[len_str-1]
+last_num = str(number)[len_str-1]
 
-if int(last_number) > 5:
-    print(f"Last digit of {number} is {last_number} and is greater than 5")
-elif int(last_number) < 6 and int(last_number) != 0:
-    print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
+if int(last_num) > 5:
+    print(f"Last digit of {number} is {last_num} and is greater than 5")
+elif int(last_num) < 6 and int(last_num) != 0:
+    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {last_number} and is 0")
+    print(f"Last digit of {number} is {last_num} and is 0")
