23c7bd2cd635a87a2a8d402b191aea28b33b3a3d
ckchesire <christianck.chesire@gmail.com>
2024-09-19 06:10:31 +0300

    Fix add function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     if a_dictionary:
         new_dictionary = a_dictionary.copy()
         for k, v in new_dictionary.items():
-            v * 2
+            new_dictionary[k] = v * 2
         return (new_dictionary)
