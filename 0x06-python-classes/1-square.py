5416e01e2b5e14f4e4456ac5c4a4f8e1d0794f32
ckchesire <christianck.chesire@gmail.com>
2024-10-01 23:30:28 +0300

    Implemented Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Created an empty class representing a Square
+    """
+    def __init__(self, size):
+        """The __init__ method that instantiates the
+        parameters required for class Square.
+
+        Args:
+            size (int): The size of a square
+        """
+        self.__size = size
