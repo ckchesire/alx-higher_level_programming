57773156b0e2526da0e5f3bec14e3f04c65e4218
ckchesire <christianck.chesire@gmail.com>
2024-10-02 03:55:50 +0300

    Updated code04: implemented code to add coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,11 +79,13 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            for y in range(self.__position[1]):
-                print()
+            if self.__position[1] > 0:
+                for y in range(self.__position[1]):
+                    print()
             for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(' ', end='')
+                if self.__position[0] > 0:
+                    for x in range(self.__position[0]):
+                        print(' ', end='')
                 for j in range(self.__size):
                     print('#', end='')
                 print()
