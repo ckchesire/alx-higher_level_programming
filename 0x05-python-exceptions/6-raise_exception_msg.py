e679bf3896cd6e87f09cf40d552c4b927eb20245
ckchesire <christianck.chesire@gmail.com>
2024-10-03 00:33:51 +0300

    Implemented function that raises a name exception with a message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Function used to raise name exception with a message
+
+        Args:
+            message: The message to be displayed for exception
+    """
+    raise NameError(message)
