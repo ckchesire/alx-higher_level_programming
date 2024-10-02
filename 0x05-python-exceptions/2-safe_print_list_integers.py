2af0e393c1c23417945ed194f352890fb922373e
ckchesire <christianck.chesire@gmail.com>
2024-10-02 20:25:42 +0300

    Added a new function to print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """ Function that prints x elements of a list. If x exceeds
+        the length it prints the passed elements without raising
+        an error. It also checks if value is an integer if it is
+        an integer it gets printed, else the value is skipped.
+
+        Args:
+            my_list: Elements in a list to be printed
+            x (int): No. of elements to be printed
+
+        Returns:
+            No. of elements printed
+    """
+    count = 0
+
+    try:
+        for i in range(0, x):
+            if isinstance(my_list[i], int):
+                print(my_list[i], end="")
+                count += 1
+    except ValueError:
+        pass
+    except IndexError:
+        pass
+    finally:
+        print()
+
+    return count
