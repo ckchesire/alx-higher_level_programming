1ac499a21a3cf20a6f6b3dd9252a13f061637a1a
ckchesire <christianck.chesire@gmail.com>
2024-10-07 20:48:34 +0300

    1.Implemented instance attributes for class Rectangle that defines a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-""" Defines a class Rectangle. """
+""" Defines a class Rectangle.
+
+    Attributes:
+        height: An integer value representing the height of a rectangle
+        width: An integer value representing the width of a rectangle
+"""
 
 
 class Rectangle:
     """ Class to define Rectangle Blueprint. """
 
     def __init__(self, width=0, height=0):
-        """ Initializes the data values """
+        """ Initializes the data values of a rectange
+
+            Args:
+                width: int value for width default being 0
+                height: int value for height default being 0
+        """
         self.__height = height
         self.__width = width
 
     @property
     def height(self):
+        """ Getter to retrieve height private attribute value
+
+            Returns:
+                returns the height private attribute
+        """
         return self.__height
 
     @height.setter
     def height(self, height):
+        """Setter sets the height private attribute value.
+
+            Validates the assignment of the height private attribute.
+
+        Arg:
+            height: the value to be set
+        """
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height < 0:
@@ -24,10 +46,22 @@ class Rectangle:
 
     @property
     def width(self):
+        """ Getter to retrieve width private attribute value
+
+            Returns:
+                returns the width private attribute
+        """
         return self.__width
 
     @width.setter
     def width(self, width):
+        """Setter sets the width private attribute value.
+
+            Validates the assignment of the width private attribute.
+
+        Arg:
+            height: the value to be set
+        """
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
