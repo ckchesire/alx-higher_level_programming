17ec1de88ced09f7a22c6a823de766ddc4cf1b19
ckchesire <christianck.chesire@gmail.com>
2024-10-07 19:51:13 +0300

    Adde 0-rectangle blueprint

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-""" Defines a class Rectangle. 
+""" Defines a class Rectangle.
 
-    This module contains an empty class that defines a rectangle.
-
-
-    Usage Example:
-        Rectangle = __import__('0-rectangle').Rectangle
-
-        my_rectangle = Rectangle()
-        print(type(my_rectangle))
-        print(my_rectangle.__dict__)
-
+    This module contains an empty class that defines a Rectangle.
 """
+
 
 class Rectangle:
     """ Class to define Rectangle Blueprint. """
