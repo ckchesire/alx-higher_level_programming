9f24d6916129a22b60679b67d35a47d5c02de372
ckchesire <christianck.chesire@gmail.com>
2024-10-07 22:16:44 +0300

    Added methods to calculate area and perimeter.

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+""" Defines a class Rectangle.
+
+    Attributes:
+        height: An integer value representing the height of a rectangle
+        width: An integer value representing the width of a rectangle
+"""
+
+
+class Rectangle:
+    """ Class to define Rectangle Blueprint. """
+
+    def __init__(self, width=0, height=0):
+        """ Initializes the data values of a rectange
+
+            Args:
+                width: int value for width default being 0
+                height: int value for height default being 0
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """ Getter to retrieve height private attribute value
+
+            Returns:
+                returns the height private attribute
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter sets the height private attribute value.
+
+            Validates the assignment of the height private attribute.
+
+        Arg:
+            value: the value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """ Getter to retrieve width private attribute value
+
+            Returns:
+                returns the width private attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter sets the width private attribute value.
+
+            Validates the assignment of the width private attribute.
+
+        Arg:
+            value: the value to be set
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """ Method calcuates the area of a rectangle
+
+            Returns:
+                The area of the retangle
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ Method calculates the perimetet of a rectangle
+
+            Returns:
+                The perimeter of the rectangle if both height
+                and width is greater than zero
+        """
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        return (2*self.__height) + (2*self.__width)
