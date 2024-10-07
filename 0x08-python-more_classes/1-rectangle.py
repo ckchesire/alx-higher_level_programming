9eecccc656d301a005568b093b66e5dafb3a8aed
ckchesire <christianck.chesire@gmail.com>
2024-10-07 20:27:59 +0300

    Created instance attributes for class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Defines a class Rectangle. """
+
+
+class Rectangle:
+    """ Class to define Rectangle Blueprint. """
+
+    def __init__(self, width=0, height=0):
+        """ Initializes the data values """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
