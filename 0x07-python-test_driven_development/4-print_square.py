1747542dec35213291a8f83fc56c09f549773349
ckchesire <christianck.chesire@gmail.com>
2024-10-12 01:32:07 +0300

    Function that prints a square with the character #.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" Module prints square representation """
+
+
 def print_square(size):
+    """ Function prints a square based on # symbol
+
+        Args:
+            size (int): size of the square
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
@@ -8,4 +16,4 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-            print("#"*size)
+        print("#"*size)
