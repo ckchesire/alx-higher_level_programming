d6e412ac36ab810d177132f19fcd6beaa1dd5d46
ckchesire <christianck.chesire@gmail.com>
2024-10-11 23:29:12 +0300

    Added a function that prints out a name.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+""" Module to print out a name """
+
+
 def say_my_name(first_name, last_name=""):
+    """ Function that prints out name
+
+        Args:
+            first_name (str): first name string value
+            last_name (str): last name string value
+
+        Return:
+            Method returns a print out of first name and last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
