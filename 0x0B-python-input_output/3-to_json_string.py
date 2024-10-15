e7340e9b5aa98473936fa2de95230c9561eb1f76
ckchesire <christianck.chesire@gmail.com>
2024-10-16 02:27:34 +0300

    Implemented a function that returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+""" Module to return the JSON representation of an object or string """
+
+
+def to_json_string(my_obj):
+    """Function to return the JSON rep of an object
+
+        Args:
+            my_obj: python object or string
+
+        Return:
+            returns the JSON representation
+    """
+    return json.dumps(my_obj)
