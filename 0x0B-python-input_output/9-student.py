eabeeb56e71ef5500bf802699bc23e9aec3614c7
ckchesire <christianck.chesire@gmail.com>
2024-10-16 05:41:51 +0300

    Update: 9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
+"""Module retrieves a dictionary representation of a Student instance
+"""
+
+
 class Student:
     """ Blueprint for class Student
     """
     def __init__(self, first_name, last_name, age):
-        """ Initialize instance variables for class """
+        """ Initialize instance variables for class
+
+            Args:
+                first_name : string of student's first name
+                last_name : string of student's last name
+                age : students age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """Function to retrieve attributes of student class
+
+            Returns:
+                returns a dictionary representation of student instance
         """
         return self.__dict__
