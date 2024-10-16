828bb8a0c07b65a0c95e2634bda8a31abbd1d515
ckchesire <christianck.chesire@gmail.com>
2024-10-16 04:48:54 +0300

    Added a function that returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module to convert Class to JSON """
+
+
+import json
+
+
+def class_to_json(obj):
+    """Function to convert Class to JSON
+
+        Args:
+            obj : class object to be converted
+
+        Returns:
+            returns the dictionary description
+    """
+    res = {}
+    for k, v in obj.__dict__.items():
+        if isinstance(v, (list, dict, str, int, bool)):
+            res[k] = v
+    return res
