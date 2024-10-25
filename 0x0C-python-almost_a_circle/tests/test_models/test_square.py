b41f62570ade4beac033e3ebad987a433f1f51d0
ckchesire <christianck.chesire@gmail.com>
2024-10-25 17:17:52 +0300

    Update on 11. Square size

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,11 +2,19 @@
 
 import unittest
 from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
 from io import StringIO
 from unittest.mock import patch
 
 
 class TestSquare(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_inheritance(self):
+        self.assertTrue(issubclass(Square, Rectangle))
+
     def test_square_initialization(self):
         """ Test valid initialization """
         with self.assertRaises(TypeError):
@@ -23,7 +31,7 @@ class TestSquare(unittest.TestCase):
     def test_square_str(self):
         """Test the __str__ method output of Square."""
         sq = Square(10, 2, 3, 1)
-        expected_str = "[Square] (1) 2/3 - 10/10"
+        expected_str = "[Square] (1) 2/3 - 10"
         self.assertEqual(str(sq), expected_str)
 
     def test_square_area(self):
@@ -36,3 +44,11 @@ class TestSquare(unittest.TestCase):
         sq = Square(15, 2, 3, 1)
         self.assertEqual(sq.width, 15)
         self.assertEqual(sq.height, 15)
+
+    def test_display_with_position(self):
+        """Test display with position"""
+        s = Square(2, 2)
+        expected_output = "  ##\n  ##\n"
+        with patch('sys.stdout', new=StringIO()) as simulate_out:
+            s.display()
+            self.assertEqual(simulate_out.getvalue(), expected_output)
