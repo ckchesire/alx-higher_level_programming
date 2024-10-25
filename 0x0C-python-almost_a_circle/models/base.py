73fad50e6c337357009159c3f9ddc055956baed1
ckchesire <christianck.chesire@gmail.com>
2024-10-25 22:51:24 +0300

    update Base class by adding the class method def load_from_file(cls): that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Creating the base class module """
 
+import os
 import json
 
 
@@ -17,6 +18,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Function to return Json string
 
@@ -35,4 +37,76 @@ class Base:
 
         if not all(isinstance(d, dict) for d in list_dictionaries):
             raise TypeError("All elements must be dictionaries")
+
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): A list of instances that inherits from Base
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+
+        json_list = []
+        for obj in list_objs:
+            json_list.append(obj.to_dictionary())
+
+        json_string = cls.to_json_string(json_list)
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Function to return the list of the JSON string representation
+
+        Args:
+            json_string (str): A string representing a list of dictionaries.
+
+        Returns:
+            list: The list represented by json_string or an empty list if
+            json_string is None or empty.
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Function to return an instance with all attributes already set
+
+        Args:
+            dictionary (dict): dictionary of attributes to set.
+
+        Returns:
+            instance: returns an instance of the class with attributes
+            set according to the dictionary.
+        """
+        if dictionary is None:
+            return None
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(3, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(2)
+        else:
+            return None
+
+        dummy_instance.update(**dictionary)
+
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Function Returns a list of instances from a JSON file."""
+        filename = cls.__name__ + ".json"
+        if not os.path.isfile(filename):
+            return []
+
+        with open(filename, "r", encoding="utf-8") as f:
+            json_string = f.read()
+            list_dicts = cls.from_json_string(json_string)
+
+        return [cls.create(**d) for d in list_dicts]
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,10 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from unittest.mock import mock_open, patch
+import json
 
 
 class BaseTestCase(unittest.TestCase):
@@ -61,6 +65,147 @@ class BaseTestCase(unittest.TestCase):
     def test_to_json_string_empty_list(self):
         """ Test to_json_string with non-empty and empty list"""
         self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_save_to_file_none(self):
+        """Test save_to_file with None"""
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_save_to_file_empty_list(self):
+        """Test save_to_file with empty list"""
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_save_to_file_rectangle(self):
+        """Test save_to_file with Rectangle instances"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+    def test_save_to_file_square(self):
+        """Test save_to_file with Square instances"""
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        Square.save_to_file([s1, s2])
+
+    def test_from_json_string_invalid(self):
+        """Test from_json_string with None input,  empty string and
+            empty list
+        """
+        result = Base.from_json_string(None)
+        self.assertEqual(result, [])
+        self.assertIsInstance(result, list)
+
+        result1 = Base.from_json_string("")
+        self.assertEqual(result1, [])
+        self.assertIsInstance(result1, list)
+
+        result2 = Base.from_json_string("[]")
+        self.assertEqual(result2, [])
+        self.assertIsInstance(result2, list)
+
+    def test_from_json_string(self):
+        """Test from_json_string with JSON string for rectangle and square"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_string = Base.to_json_string([dictionary])
+        result = Base.from_json_string(json_string)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0], dict)
+        self.assertEqual(result[0]['width'], 10)
+        self.assertEqual(result[0]['height'], 7)
+        self.assertEqual(result[0]['x'], 2)
+        self.assertEqual(result[0]['y'], 8)
+
+        s1 = Square(5, 1, 2)
+        dictionary = s1.to_dictionary()
+        json_string = Base.to_json_string([dictionary])
+        result = Base.from_json_string(json_string)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0], dict)
+        self.assertEqual(result[0]['size'], 5)
+        self.assertEqual(result[0]['x'], 1)
+        self.assertEqual(result[0]['y'], 2)
+
+    def test_create_rectangle_and_square(self):
+        """Test that create method correctly creates a Rectangle and square
+            instance with given attributes."""
+        attrs = {"id": 10, "width": 4, "height": 6, "x": 2, "y": 3}
+        r_instance = Rectangle.create(**attrs)
+
+        self.assertIsInstance(r_instance, Rectangle)
+        self.assertEqual(r_instance.id, 10)
+        self.assertEqual(r_instance.width, 4)
+        self.assertEqual(r_instance.height, 6)
+        self.assertEqual(r_instance.x, 2)
+        self.assertEqual(r_instance.y, 3)
+
+        attrs = {"id": 15, "size": 5, "x": 1, "y": 2}
+        s_instance = Square.create(**attrs)
+
+        self.assertIsInstance(s_instance, Square)
+        self.assertEqual(s_instance.id, 15)
+        self.assertEqual(s_instance.size, 5)
+        self.assertEqual(s_instance.width, 5)
+        self.assertEqual(s_instance.height, 5)
+        self.assertEqual(s_instance.x, 1)
+        self.assertEqual(s_instance.y, 2)
+
+    @patch("models.base.os.path.isfile", return_value=True)
+    @patch("models.base.open", new_callable=mock_open, read_data='[{"id": 1,\
+            "width": 5, "height": 5, "x": 0, "y": 0}]')
+    def test_load_from_file_rectangle(self, mock_open, mock_isfile):
+        """Test for Rectangle class when file is presenr and has valid data."""
+        rects = Rectangle.load_from_file()
+        self.assertEqual(len(rects), 1)
+        self.assertIsInstance(rects[0], Rectangle)
+        self.assertEqual(rects[0].id, 1)
+        self.assertEqual(rects[0].width, 5)
+        self.assertEqual(rects[0].height, 5)
+        self.assertEqual(rects[0].x, 0)
+        self.assertEqual(rects[0].y, 0)
+
+    @patch("models.base.os.path.isfile", return_value=True)
+    @patch("models.base.open", new_callable=mock_open, read_data='[{"id": 1, \
+            "size": 3, "x": 1, "y": 1}]')
+    def test_load_from_file_square(self, mock_open, mock_isfile):
+        """Test for Square class when file is present and has valid data."""
+        squares = Square.load_from_file()
+        self.assertEqual(len(squares), 1)
+        self.assertIsInstance(squares[0], Square)
+        self.assertEqual(squares[0].id, 1)
+        self.assertEqual(squares[0].size, 3)
+        self.assertEqual(squares[0].x, 1)
+        self.assertEqual(squares[0].y, 1)
+
+    @patch("models.base.os.path.isfile", return_value=False)
+    def test_load_from_file_no_file(self, mock_isfile):
+        """Test for when file does is not there, should return an
+            empty list.
+        """
+        rects = Rectangle.load_from_file()
+        self.assertEqual(rects, [])
+
+    @patch("models.base.os.path.isfile", return_value=True)
+    @patch("models.base.open", new_callable=mock_open, read_data='[]')
+    def test_load_from_file_empty_file(self, mock_open, mock_isfile):
+        """Test for an empty JSON file, should return an empty list."""
+        rects = Rectangle.load_from_file()
+        self.assertEqual(rects, [])
+
+    @patch("models.base.os.path.isfile", return_value=True)
+    @patch("models.base.open", new_callable=mock_open, read_data='invalid\
+            json')
+    def test_load_from_file_invalid_json(self, mock_open, mock_isfile):
+        """Test for invalid JSON data, should raise a JSONDecodeError."""
+        with self.assertRaises(json.JSONDecodeError):
+            Rectangle.load_from_file()
 
 
 if __name__ == '__main__':
