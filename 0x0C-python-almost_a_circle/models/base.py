8a6b0cc8561fd50c564a1acee3163d75c441c996
ckchesire <christianck.chesire@gmail.com>
2024-10-25 21:05:04 +0300

    Update: Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,15 @@ class Base:
             self.id = id
 
     def to_json_string(list_dictionaries):
-        """ Function to return Json string """
-        if list_dictionaries is None:
+        """ Function to return Json string
+
+            Args:
+                list_dictionaries (list): List of dictionaries to convert
+
+            Returns:
+                str: JSON string representation of list_dictionaries.
+                Returns "[]" if list_dictionaries is None or empty
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
