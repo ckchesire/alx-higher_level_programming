def4a1e61fa5d1fbf49c2f6324a7d780de816fb8
Christian Chesire <christianck.chesire@gmail.com>
2024-12-18 11:22:54 -0800

    Update added script that prints two arguments passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -5,10 +5,4 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 const secondArg = args[1];
 
-if (firstArg !== undefined && secondArg !== undefined) {
-  console.log(`${firstArg} is ${secondArg}`);
-} else if (firstArg !== undefined && secondArg === undefined) {
-  console.log(`${firstArg} is undefined`);
-} else if (firstArg === undefined && secondArg === undefined) {
-  console.log(`undefined is undefined`);
-}
+console.log(`${firstArg || 'undefined'} is ${secondArg || 'undefined'}`);
