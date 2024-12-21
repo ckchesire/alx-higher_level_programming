96825e26cfba44d9e71e6c6695170e45c2994697
Christian Chesire <christianck.chesire@gmail.com>
2024-12-20 18:17:52 -0800

    Updated function that returns the reversed version of a list.

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const newlist = list.reverse();
+  const newlist = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newlist.push(list[i]);
+  }
   return newlist;
 };
