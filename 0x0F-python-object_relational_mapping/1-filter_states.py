21fec5b6f2d2537ecccc4a19a3ff87648ead329b
ckchesire <christianck.chesire@gmail.com>
2024-12-31 05:16:22 -0800

    Implemented code that filters all states.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module that lists all states with N from the database
+"""
+
+import MySQLdb
+import sys
+
+
+def get_states_with_n():
+    """
+        Method to that retrieves command-line arguments
+        and runs query on mysql server instance
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    get_states_with_n()
